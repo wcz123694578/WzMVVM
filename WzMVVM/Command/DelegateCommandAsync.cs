f933e1da6251a26916062fe072e39e0c471f0baf
wcz <wcz123694578@outlook.com>
2025-08-25 20:12:51 +0800

    增加异步命令

---
+++ WzMVVM/Command/DelegateCommandAsync.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WzMVVM.Command
+{
+    /// <summary>
+    /// 异步 DelegateCommand（无参数）
+    /// </summary>
+    public class DelegateCommandAsync : ICommand
+    {
+        private readonly Func<Task> _execute;
+        private readonly Func<bool> _canExecute;
+        private bool _isExecuting;
+
+        public DelegateCommandAsync(Func<Task> execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_isExecuting) return false;
+            return _canExecute?.Invoke() ?? true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            try
+            {
+                _isExecuting = true;
+                RaiseCanExecuteChanged();
+
+                await _execute();
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    /// <summary>
+    /// 异步 DelegateCommand（带参数）
+    /// </summary>
+    public class DelegateCommandAsync<T> : ICommand
+    {
+        private readonly Func<T, Task> _execute;
+        private readonly Func<T, bool> _canExecute;
+        private bool _isExecuting;
+
+        public DelegateCommandAsync(Func<T, Task> execute, Func<T, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_isExecuting) return false;
+
+            if (_canExecute == null) return true;
+
+            if (parameter == null && typeof(T).IsValueType)
+                return false;
+
+            return parameter is T t ? _canExecute(t) : _canExecute(default(T));
+        }
+
+        public async void Execute(object parameter)
+        {
+            try
+            {
+                _isExecuting = true;
+                RaiseCanExecuteChanged();
+
+                if (parameter is T t)
+                    await _execute(t);
+                else if (parameter == null && !typeof(T).IsValueType)
+                    await _execute(default(T));
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
